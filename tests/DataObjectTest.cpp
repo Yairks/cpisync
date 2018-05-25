63b0eccf842224515730423f0b241ae34c8ecd23
Eliezer Pearl <eliezer.pearl@gmail.com>
2018-05-25 11:33:35 -0400

    finish: DataObjectTest.cpp

--- tests/DataObjectTest.cpp
+++ tests/DataObjectTest.cpp
@@ -26,11 +26,11 @@ void DataObjectTest::tearDown(){
 }
 
 void DataObjectTest::testToZZAndInitZZ(){
-    const short init = 64;
-    const ZZ zz = static_cast<ZZ>(init);
+    const int _init = 64;
+    const ZZ init = static_cast<ZZ>(_init);
     
-    const DataObject d(zz);
-    CPPUNIT_ASSERT(d.to_ZZ() == zz);
+    const DataObject d(init);
+    CPPUNIT_ASSERT(d.to_ZZ() == init);
     
 }
 
@@ -48,69 +48,90 @@ void DataObjectTest::testToStringAndInitString(){
     // False case
     DataObject::RepIsInt = false;
     d = DataObject(s);
-    CPPUNIT_ASSERT(d.to_string() == s); 
+    CPPUNIT_ASSERT_EQUAL(s, d.to_string()); 
     
     // True case
     DataObject::RepIsInt = true;
     d = DataObject(t);
     
-    CPPUNIT_ASSERT(d.to_string() == t);
+    CPPUNIT_ASSERT_EQUAL(t, d.to_string());
     
     // Case when string is initialized as a bitsequence (false)
     // and then becomes an integer representation of that bitsequence (true)
     DataObject::RepIsInt = false;
     d = DataObject(s);
     DataObject::RepIsInt = true;
-    CPPUNIT_ASSERT(d.to_string() == t);
+    CPPUNIT_ASSERT_EQUAL(t, d.to_string());
     
     // Case when string is initialized as integer representation (true)
     // and then converted to a bitsequence character representation (false)
     DataObject::RepIsInt = true;
     d = DataObject(t);
     DataObject::RepIsInt = false;
-    CPPUNIT_ASSERT(d.to_string() == s);
+    CPPUNIT_ASSERT_EQUAL(s, d.to_string());
 }
 
 void DataObjectTest::testToStringAndInitEmpty(){
-    const string expected = "\0";
+    // We expect a default constructed DataObject 
+    // to be represented as an empty string.
+    const string expected = "";
+    
+    // An empty string if we assume that it represents an integer
     const string expected_int = "0";
+    
     DataObject d;
     
-    // false
+    // false case
     DataObject::RepIsInt = false;
-    CPPUNIT_ASSERT(d.to_string() == expected);
+    CPPUNIT_ASSERT_EQUAL(expected, d.to_string());
     
-    // true
+    // true case
     DataObject::RepIsInt = true;
-    CPPUNIT_ASSERT(d.to_string() == expected_int);
+    CPPUNIT_ASSERT_EQUAL(expected_int, d.to_string());
     
 }
 
 void DataObjectTest::testToPriorityStringAndInitStringableAndSetPriority(){
+    // Our initial value. Not a ZZ or a string, so the templated constructor
+    // should be called.
     const int init = 65;
+    
+    // Our initial value as a string. This represents the expected return value
+    // of DataObject::to_string(), since the templated constructor should
+    // "simply [convert] the object to a string and then [do] the same as the
+    // string constructor."
+    const string init_string = to_string(init);
+    
+    // If DataObject::RepIsInt == true, the bitwise representation of the string
+    // "65" will be considered as an integer (the equivalent of 
+    // reinterpret_cast<int>(init_string) if you were actually permitted to do
+    // this)
+    const int init_string_as_int = 13622;
+    
+    // ZZ representing an arbitrary priority, 54.
     const int _priority = 54;
     const ZZ priority = static_cast<ZZ>(_priority);
-    //DataObject d(init); // Shouldn't this work? Not sure what I'm doing wrong here. Integers are definitely "stringable"
-    DataObject d(to_string(init));
+    
+    DataObject d(init);
     
     d.setPriority(priority);
-    // false
-    DataObject::RepIsInt = false;
-    const string init_string = "A";
-    CPPUNIT_ASSERT(d.to_priority_string() == init_string);
     
-    // true
+    // false case
+    DataObject::RepIsInt = false;
+    CPPUNIT_ASSERT_EQUAL(init_string, d.to_priority_string());
+    
+    // true case
     DataObject::RepIsInt = true;
     stringstream expected;
-    expected << priority << "," << init;
-    CPPUNIT_ASSERT(d.to_priority_string() == expected.str());
+    expected << priority << "," << init_string_as_int;
+    CPPUNIT_ASSERT_EQUAL(expected.str(), d.to_priority_string());
     
     
 }
 
 void DataObjectTest::testToCharArray(){
     const string s = "AA";    
-    const short expected_len = 2; // "AA" is 2 chars long
+    int expected_len = s.length();
     const char *expected = s.data();
     
     DataObject::RepIsInt = false;  
@@ -119,13 +140,18 @@ void DataObjectTest::testToCharArray(){
     int result_len;
     const char *result = d.to_char_array(result_len);
     
+    CPPUNIT_ASSERT_EQUAL(expected_len, result_len);
+    
+    // assert equality of char* arrays by checking equality of
+    // each corresponding element. and since we already asserted
+    // that expected_len == result_len, we shouldn't expect to access
+    // out-of-bounds values.
     bool same = true;
     for(int i = 0; i < result_len; i++){
         if(result[i] != expected[i])
             same = false;
     }
     
-    CPPUNIT_ASSERT(expected_len == result_len);
     CPPUNIT_ASSERT(same);
     
 }
@@ -140,12 +166,12 @@ void DataObjectTest::testPrint(){
     // false
     DataObject::RepIsInt = false;
     d = DataObject(s);
-    CPPUNIT_ASSERT(d.print() == s_b64);
+    CPPUNIT_ASSERT_EQUAL(d.print(), s_b64);
     
     // true
     DataObject::RepIsInt = true;
     d = DataObject(s_int);
-    CPPUNIT_ASSERT(d.print() == s_int);
+    CPPUNIT_ASSERT_EQUAL(d.print(), s_int);
 }
 
 void DataObjectTest::testStreamInsertion(){
@@ -159,7 +185,7 @@ void DataObjectTest::testStreamInsertion(){
     expected << s;
     result << d;
     
-    CPPUNIT_ASSERT(expected.str() == result.str());
+    CPPUNIT_ASSERT_EQUAL(expected.str(), result.str());
 }
 
 void DataObjectTest::testLessThan(){
@@ -178,7 +204,7 @@ void DataObjectTest::testPriority(){
     const ZZ priority = static_cast<ZZ>(_priority);
     DataObject d;
     d.setPriority(priority);
-    CPPUNIT_ASSERT(d.getPriority() == priority);
+    CPPUNIT_ASSERT_EQUAL(d.getPriority(), priority);
     
 }
 
@@ -186,5 +212,5 @@ void DataObjectTest::testTimeStamp(){
     const clock_t time = 12345;
     DataObject d;
     d.setTimeStamp(time);
-    CPPUNIT_ASSERT(d.getTimeStamp() == time);
+    CPPUNIT_ASSERT_EQUAL(d.getTimeStamp(), time);
 }
