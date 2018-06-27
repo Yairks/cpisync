c92501ffdad0eea8d5f9222035c23141eae8fbeb
Eliezer Pearl <eliezer.pearl@gmail.com>
2018-06-27 09:55:39 -0400

    add: initial tests for FullSync class

---
+++ tests/FullSyncTest.cpp
@@ -0,0 +1,23 @@
+/* This code is part of the CPISync project developed at Boston University.  Please see the README for use and references. */
+
+/* 
+ * File:   FullSyncTest.cpp
+ * Author: kaets
+ * 
+ * Created on June 27, 2018, 9:49 AM
+ */
+
+#include "FullSyncTest.h"
+
+FullSyncTest::FullSyncTest() {
+}
+
+FullSyncTest::~FullSyncTest() {
+}
+
+void FullSyncTest::setUp() { 
+}
+
+void FullSyncTest::tearDown() { 
+}
+
