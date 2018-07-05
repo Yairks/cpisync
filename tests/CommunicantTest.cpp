43a479df6370f99dae9ad86ef2acc1c36fb2e2ff
Eliezer Pearl <eliezer.pearl@gmail.com>
2018-07-05 15:31:34 -0400

    finish: communicant tests

--- tests/CommunicantTest.cpp
+++ tests/CommunicantTest.cpp
@@ -17,34 +17,43 @@
 
 CPPUNIT_TEST_SUITE_REGISTRATION(CommunicantTest);
 
-// Generates `amt` random integers from the seeded prng
-vector<int> genRandInt(int amt) {
-    vector<int> vals;
-    
-    for(int ii = 0; ii < amt; ii++)
-        vals.push_back(rand());
-    
-    return vals;
+inline int randLenBetween(int lower, int upper) {
+    int length = (rand() % (upper + 1));
+    if(length < lower) length = lower;
+    return length;
 }
 
-// Generates `amt` random strings with length chosen random s.t. `lower` <= length <= `upper`
-vector<string> genRandString(int amt, int lower, int upper) {
-    vector <string> vals;
-    
-    for(int ii = 0; ii < amt; ii++) {
-        stringstream str;
-        
-        // pick a length in between lower and upper, inclusive
-        int length = (rand() % (upper + 1));
-        if(length < lower) length = lower;
-        
-        for(int jj = 0; jj < length; jj++)
-            str << (char) (rand() % (int) ((pow(2, CHAR_BIT)-1))); // generate a random character and add to stringstream
-        
-        vals.push_back(str.str());
+inline long randLong() {
+    if(sizeof(int) < sizeof(long)) {
+        return static_cast<long>(rand());
+    } else {
+        return (static_cast<long>(rand()) << (sizeof(int) * CHAR_BIT)) | rand(); // lshift the amount of bits in an int and then bitwise or a random int
     }
-    
-    return vals;
+}
+
+inline byte randByte() {
+    return (char) (rand() % 256);
+}
+
+
+inline string randString(int lower, int upper) {
+    stringstream str;
+
+    // pick a length in between lower and upper, inclusive
+    int length = randLenBetween(lower, upper);
+
+    for(int jj = 0; jj < length; jj++)
+        str << randByte(); // generate a random character and add to stringstream
+
+    return str.str();
+}
+
+inline string randIntString() {
+    return to_string(rand());
+}
+
+inline double randDouble(double lower, double upper) {
+    return ( ( double )rand() * ( upper - lower ) ) / ( double )RAND_MAX + lower;
 }
 
 CommunicantTest::CommunicantTest() {
@@ -80,33 +89,40 @@ void CommunicantTest::testBytesAndResetCommCounters() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
     const long int NONE = 0;
-    const string STR = "hello world";
-    const long int STR_LENGTH = STR.length();
+    const int SIZE = 100;
+    const int LOWER_BOUND = 0;
+    const int UPPER_BOUND = 50;
     
-    CPPUNIT_ASSERT_EQUAL(NONE, cSend.getXmitBytesTot());
-    CPPUNIT_ASSERT_EQUAL(NONE, cRecv.getRecvBytesTot());
+    long int expXmitTot = 0;
+    long int expRecvTot = 0;
     
-    cSend.resetCommCounters();
-    cRecv.resetCommCounters();
-    
-    CPPUNIT_ASSERT_EQUAL(NONE, cSend.getXmitBytes());
-    CPPUNIT_ASSERT_EQUAL(NONE, cRecv.getRecvBytes());
-            
-    cSend.commSend(STR.data(), STR_LENGTH);
-    
-    CPPUNIT_ASSERT_EQUAL(STR_LENGTH, cSend.getXmitBytes());
-    CPPUNIT_ASSERT_EQUAL(STR_LENGTH, cSend.getXmitBytesTot());
-    
-    cRecv.commRecv(STR.length());
-    
-    CPPUNIT_ASSERT_EQUAL(STR_LENGTH, cRecv.getRecvBytes());
-    CPPUNIT_ASSERT_EQUAL(STR_LENGTH, cRecv.getRecvBytesTot());
-    
-    cSend.resetCommCounters();
-    cRecv.resetCommCounters();
-    
-    CPPUNIT_ASSERT_EQUAL(NONE, cRecv.getRecvBytes());
-    CPPUNIT_ASSERT_EQUAL(NONE, cSend.getXmitBytes());
+    for(int ii = 0; ii < SIZE; ii++) {
+        const string TO_SEND = randString(LOWER_BOUND, UPPER_BOUND);
+        const long int STR_LENGTH = TO_SEND.length();
+        
+        CPPUNIT_ASSERT_EQUAL(expXmitTot, cSend.getXmitBytesTot());
+        CPPUNIT_ASSERT_EQUAL(expRecvTot, cRecv.getRecvBytesTot());
+        
+        cSend.resetCommCounters();
+        cRecv.resetCommCounters();
+        
+        CPPUNIT_ASSERT_EQUAL(NONE, cSend.getXmitBytes());
+        CPPUNIT_ASSERT_EQUAL(NONE, cRecv.getRecvBytes());
+        
+        cSend.commSend(TO_SEND.data(), STR_LENGTH);
+        
+        expXmitTot += STR_LENGTH;
+        
+        CPPUNIT_ASSERT_EQUAL(STR_LENGTH, cSend.getXmitBytes());
+        CPPUNIT_ASSERT_EQUAL(expXmitTot, cSend.getXmitBytesTot());
+        
+        cRecv.commRecv(STR_LENGTH);
+        
+        expRecvTot += STR_LENGTH;
+        
+        CPPUNIT_ASSERT_EQUAL(STR_LENGTH, cRecv.getRecvBytes());
+        CPPUNIT_ASSERT_EQUAL(expRecvTot, cRecv.getRecvBytesTot());
+    }
 }
 
 void CommunicantTest::testEstablishModAndCommZZ_p() {
@@ -117,11 +133,9 @@ void CommunicantTest::testEstablishModAndCommZZ_p() {
     
     // Generate two sets of 100 random mods and ZZ_p values
     const int SIZE = 100;
-    const vector<int> TEST_VALUES = genRandInt(SIZE);
-    const vector<int> TEST_MODS = genRandInt(SIZE);
     
     for(int ii = 0; ii < SIZE; ii++) {
-        const ZZ MOD(TEST_MODS[ii]);
+        const ZZ MOD(rand());
         ZZ_p::init(MOD);
     
         // Tests that establishMod works with oneWay set to true.
@@ -129,7 +143,7 @@ void CommunicantTest::testEstablishModAndCommZZ_p() {
         CPPUNIT_ASSERT(cSend.establishModSend(ONE_WAY)); 
         CPPUNIT_ASSERT(cRecv.establishModRecv(ONE_WAY));
         
-        const ZZ_p EXP(TEST_VALUES[ii]);
+        const ZZ_p EXP(rand());
         cSend.Communicant::commSend(EXP);
         const ZZ_p RES = cRecv.commRecv_ZZ_p();
         
@@ -146,10 +160,9 @@ void CommunicantTest::testCommUstringBytes() {
     const int LOWER_BOUND = 1;
     const int UPPER_BOUND = 250; // arbitrary upper bound
     
-    const vector<string> TEST_VALUES = genRandString(SIZE, LOWER_BOUND, UPPER_BOUND);
-    
     for(int ii = 0; ii < SIZE; ii++) {
-        const ustring EXP = (const unsigned char *) (TEST_VALUES[ii].data());
+        const string SS = randString(LOWER_BOUND, UPPER_BOUND);
+        const ustring EXP = (const unsigned char *) (SS.data());
         const int EXP_LEN = EXP.size();
 
         // check that transmitted bytes are successfully incremented 
@@ -173,10 +186,9 @@ void CommunicantTest::testCommUstringNoBytes() {
     const int LOWER_BOUND = 1;
     const int UPPER_BOUND = 25; // arbitrary upper bound
     
-    const vector<string> TEST_VALUES = genRandString(SIZE, LOWER_BOUND, UPPER_BOUND);
-    
     for(int ii = 0; ii < SIZE; ii++) {
-        const ustring EXP = (const unsigned char *) (TEST_VALUES[ii].data());
+        const string SS = randString(LOWER_BOUND, UPPER_BOUND);
+        const ustring EXP = (const unsigned char *) (SS.data());
         cSend.Communicant::commSend(EXP);
         const ustring RES = cRecv.commRecv_ustring();
     
@@ -189,12 +201,18 @@ void CommunicantTest::testCommString() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
     
-    const string EXP = "ghijk";
+    const int SIZE = 100;
+    const int LOWER_BOUND = 1;
+    const int UPPER_BOUND = 25;
     
-    cSend.Communicant::commSend(EXP);
-    const string RES = cSend.commRecv_string();
     
-    CPPUNIT_ASSERT_EQUAL(EXP, RES);
+    for(int ii = 0; ii < SIZE; ii++) {
+        const string EXP = randString(LOWER_BOUND, UPPER_BOUND);
+        cSend.Communicant::commSend(EXP);
+        const string RES = cRecv.commRecv_string();
+        
+        CPPUNIT_ASSERT_EQUAL(EXP, RES);
+    }
 }
 
 void CommunicantTest::testCommLong() {
@@ -202,12 +220,14 @@ void CommunicantTest::testCommLong() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
     
-    const long int EXP = 1234567890;
-    cSend.Communicant::commSend(EXP);
-   
-    const long int RES = cRecv.commRecv_long();
-
-    CPPUNIT_ASSERT_EQUAL(EXP, RES);
+    const int SIZE = 100;
+    
+    for(int ii = 0; ii < SIZE; ii++){
+        const long EXP = randLong();
+        cSend.Communicant::commSend(EXP);
+        const long RES = cRecv.commRecv_long();
+        CPPUNIT_ASSERT_EQUAL(EXP, RES);
+    }
 }
 
 void CommunicantTest::testCommDataObject() {
@@ -215,13 +235,19 @@ void CommunicantTest::testCommDataObject() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
 
-    DataObject exp(string("abcdef"));
-    cSend.Communicant::commSend(exp);
-    DataObject* res = cRecv.commRecv_DataObject();
-
-    // same as testing for equality.
-    CPPUNIT_ASSERT(!(exp < *res || *res < exp));
-    CPPUNIT_ASSERT_EQUAL(exp.getPriority(), res->getPriority());
+    const int SIZE = 100;
+    const int LOWER_BOUND = 1;
+    const int UPPER_BOUND = 25;
+    
+    for(int ii = 0; ii < SIZE; ii++){
+        const string SS = randString(LOWER_BOUND, UPPER_BOUND);
+        DataObject exp(SS);
+        cSend.Communicant::commSend(exp);
+        DataObject* res = cRecv.commRecv_DataObject();
+        
+        // same as testing for equality.
+        CPPUNIT_ASSERT(!(exp < *res || *res < exp));
+    }
 }
 
 void CommunicantTest::testCommDataObjectPriority() { // fix this test so that the repisint doesnt need to be changed
@@ -230,31 +256,38 @@ void CommunicantTest::testCommDataObjectPriority() { // fix this test so that th
     CommDummy cRecv(&qq);
 
     const bool SEND_PRIORITY = true;
-    const string INIT = "65";
+    
+    const int SIZE = 100;
+    
     // Test case where RepIsInt is true
     DataObject::RepIsInt = true;
 
-    DataObject expA(INIT);
-    const ZZ PRIORITY(5l);
-    expA.setPriority(PRIORITY);
-   
-    cSend.Communicant::commSend(expA, SEND_PRIORITY);
-    DataObject* resA = cRecv.commRecv_DataObject_Priority();
-
-    // same as testing for equality.
-    CPPUNIT_ASSERT(!(expA < *resA || *resA < expA));
-    CPPUNIT_ASSERT_EQUAL(expA.getPriority(), resA->getPriority());
-            
+    for(int ii = 0; ii < SIZE; ii++) {
+        const ZZ PRIORITY(randLong());
+        DataObject exp(randIntString());
+        exp.setPriority(PRIORITY);
+        
+        cSend.Communicant::commSend(exp, SEND_PRIORITY);
+        DataObject* res = cRecv.commRecv_DataObject_Priority();
+        // same as testing for equality.
+        CPPUNIT_ASSERT(!(exp < *res || *res < exp));
+        CPPUNIT_ASSERT_EQUAL(exp.getPriority(), res->getPriority());
+    }
+    
     // Test case where RepIsInt is false
     DataObject::RepIsInt = false;
-    DataObject expB(INIT);
-    expB.setPriority(PRIORITY);
-    cSend.Communicant::commSend(expB, SEND_PRIORITY);
-
-    DataObject* resB = cRecv.commRecv_DataObject_Priority();
-
-    CPPUNIT_ASSERT(!(expB < *resB || *resB < expB));
-    CPPUNIT_ASSERT_EQUAL(expB.getPriority(), resB->getPriority());
+    
+    for(int ii = 0; ii < SIZE; ii++) {
+        const ZZ PRIORITY(randLong());
+        DataObject exp(randLong());
+        exp.setPriority(PRIORITY);
+        
+        cSend.Communicant::commSend(exp, SEND_PRIORITY);
+        DataObject* res = cRecv.commRecv_DataObject_Priority();
+        // same as testing for equality.
+        CPPUNIT_ASSERT(!(exp < *res || *res < exp));
+        CPPUNIT_ASSERT_EQUAL(exp.getPriority(), res->getPriority());
+    }
 }
 
 void CommunicantTest::testCommDataObjectList() {
@@ -262,35 +295,39 @@ void CommunicantTest::testCommDataObjectList() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
     
-    DataObject::RepIsInt = false;
-    list<DataObject*> exp;
-    DataObject vals[] = {
-        DataObject(string("abc")),
-        DataObject(string("def")),
-        DataObject(string("ghi")),
-        DataObject(string("jkl"))
-    };
+    const int SIZE = 50;
+    const int LOWER_BOUND = 1;
+    const int UPPER_BOUND = 10;
     
-    for(DataObject& dd : vals)
-        exp.push_back(&dd);
+    for(int ii = 0; ii < SIZE; ii++){
+        int length = randLenBetween(LOWER_BOUND, UPPER_BOUND);
+        
+        list<DataObject*> exp;
+        for(int jj = 0; jj < length; jj++) {
+            DataObject* dd = new DataObject(randLong());
+            exp.push_back(dd);
+        }
+        
+        cSend.Communicant::commSend(exp);
+        const list<DataObject*> RES = cRecv.commRecv_DoList();
+        // assert same length before iterating to check their equality
+        CPPUNIT_ASSERT_EQUAL(exp.size(), RES.size());
     
-    cSend.Communicant::commSend(exp);
-    const list<DataObject*> RES = cRecv.commRecv_DoList();
-
-    // assert same length before iterating to check their equality
-    CPPUNIT_ASSERT_EQUAL(exp.size(), RES.size());
+        list<DataObject*>::const_iterator expI = exp.begin();
+        list<DataObject*>::const_iterator resI = RES.begin();
     
-    list<DataObject*>::const_iterator expI = exp.begin();
-    list<DataObject*>::const_iterator resI = RES.begin();
-    
-    for(int i = 0; i < exp.size(); i++) {
-        DataObject currExp = **expI;
-        DataObject currRes = **resI;
-        CPPUNIT_ASSERT(!(currExp < currRes || currRes < currExp)); // equality test for each DataObject
-        CPPUNIT_ASSERT_EQUAL(currExp.getPriority(), currRes.getPriority());
-        // increment both iterators
-        advance(expI, 1);
-        advance(resI, 1);
+        for(int jj = 0; jj < length; jj++) {
+            DataObject currExp = **expI;
+            DataObject currRes = **resI;
+            CPPUNIT_ASSERT(!(currExp < currRes || currRes < currExp)); // equality test for each DataObject
+            CPPUNIT_ASSERT_EQUAL(currExp.getPriority(), currRes.getPriority());
+            // increment both iterators
+            
+            advance(expI, 1);
+            advance(resI, 1);
+        }
+        for(DataObject* dop : exp)
+            delete dop;
     }
 }
 
@@ -299,12 +336,19 @@ void CommunicantTest::testCommDouble() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
     
-    const double EXP = 332343.53125;
-    cSend.Communicant::commSend(EXP);
+    const int DELTA = 0.00001; // error tolerance comparing doubles
     
-    const double RES = cRecv.commRecv_double();
+    const int SIZE = 100;
+    const double LOWER_BOUND = 0;
+    const double UPPER_BOUND = 5000;
+    for(int ii = 0; ii < SIZE; ii++) {
+        const double EXP = randDouble(LOWER_BOUND, UPPER_BOUND);
+
+        cSend.Communicant::commSend(EXP);
+        const double RES = cRecv.commRecv_double();
    
-    CPPUNIT_ASSERT_EQUAL(EXP, RES);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(EXP, RES, DELTA);
+    }
 }
 
 void CommunicantTest::testCommByte() {
@@ -312,12 +356,15 @@ void CommunicantTest::testCommByte() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
     
-    const byte EXP = '\xFF';
+    const int SIZE = 100;
+    
+    for(int ii = 0; ii < SIZE; ii++) {
+        const byte EXP = randByte();
+        cSend.Communicant::commSend(EXP);
+        const byte RES = cRecv.commRecv_byte();
 
-    cSend.Communicant::commSend(EXP);
-    const byte RES = cRecv.commRecv_byte();
-
-    CPPUNIT_ASSERT_EQUAL(EXP, RES);
+        CPPUNIT_ASSERT_EQUAL(EXP, RES);
+    }
 }
 
 void CommunicantTest::testCommInt() {
@@ -325,41 +372,48 @@ void CommunicantTest::testCommInt() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
     
-    const int EXP = 23456;
-
-    cSend.Communicant::commSend(EXP);
+    const int SIZE = 100;
     
-    const int RES = cRecv.commRecv_int();
+    for(int ii = 0; ii < SIZE; ii++) {
+        const int EXP = rand();
+        cSend.Communicant::commSend(EXP);
+        const int RES = cRecv.commRecv_int();
 
-    CPPUNIT_ASSERT_EQUAL(EXP, RES);
+        CPPUNIT_ASSERT_EQUAL(EXP, RES);
+    }
 }
 
 void CommunicantTest::testCommVec_ZZ_p() {
     queue<char> qq;
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
-    
-    // Set moduli
-    const bool ONE_WAY = true;
-    const ZZ MODULUS(667l);
-    ZZ_p::init(MODULUS);
-    cSend.establishModSend(ONE_WAY);
-    cRecv.establishModRecv(ONE_WAY);
-    
-    const ZZ_p VALS[] = {
-        static_cast<ZZ_p>(123),
-        static_cast<ZZ_p>(456),
-        static_cast<ZZ_p>(789)
-    };
-    vec_ZZ_p exp;
-    
-    for(ZZ_p val : VALS)
-        exp.append(val);
-   
-    cSend.Communicant::commSend(exp);
 
-    const vec_ZZ_p RES = cRecv.commRecv_vec_ZZ_p();
-    CPPUNIT_ASSERT_EQUAL(exp, RES);
+    // Generate two sets of 100 random mods and veczzp values
+    const int SIZE = 100;
+    const int LOWER_BOUND = 1;
+    const int UPPER_BOUND = 10;
+    
+    for(int ii = 0; ii < SIZE; ii++) {
+        const ZZ MOD(rand());
+        ZZ_p::init(MOD);
+    
+        // Tests that establishMod works with oneWay set to true.
+        const bool ONE_WAY = true;
+        cSend.establishModSend(ONE_WAY);
+        cRecv.establishModRecv(ONE_WAY);
+        
+        // pick a length in between lower and upper, inclusive
+        int length = randLenBetween(LOWER_BOUND, UPPER_BOUND);
+        
+        vec_ZZ_p exp;
+        for(int jj = 0; jj < length; jj++)
+            exp.append(ZZ_p(randLong()));
+        
+        cSend.Communicant::commSend(exp);
+        const vec_ZZ_p RES = cRecv.commRecv_vec_ZZ_p();
+        
+        CPPUNIT_ASSERT_EQUAL(exp, RES);
+    }
 }
 
 void CommunicantTest::testCommZZ() {
@@ -367,13 +421,17 @@ void CommunicantTest::testCommZZ() {
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
 
-    const ZZ EXP(123456l);
-    const int EXP_SIZE = sizeof(EXP);
+    const int SIZE = 100;
+    
+    for(int ii = 0; ii < SIZE; ii++) {
+        const ZZ EXP(randLong());
+        const int EXP_SIZE = sizeof(EXP);
 
-    cSend.Communicant::commSend(EXP, EXP_SIZE);
-    const ZZ RES = cRecv.commRecv_ZZ(EXP_SIZE);
+        cSend.Communicant::commSend(EXP, EXP_SIZE);
+        const ZZ RES = cRecv.commRecv_ZZ(EXP_SIZE);
 
-    CPPUNIT_ASSERT_EQUAL(EXP, RES);
+        CPPUNIT_ASSERT_EQUAL(EXP, RES);
+    }
 }
 
 void CommunicantTest::testCommZZNoArgs(){
@@ -381,9 +439,14 @@ void CommunicantTest::testCommZZNoArgs(){
     CommDummy cSend(&qq);
     CommDummy cRecv(&qq);
 
-    const ZZ EXP(123456l);
-    cSend.Communicant::commSend(EXP);
-    const ZZ RES = cRecv.commRecv_ZZ();
+    const int SIZE = 100;
+    
+    for(int ii = 0; ii < SIZE; ii++) {
+        const ZZ EXP(randLong());
 
-    CPPUNIT_ASSERT_EQUAL(EXP,RES);
+        cSend.Communicant::commSend(EXP);
+        const ZZ RES = cRecv.commRecv_ZZ();
+
+        CPPUNIT_ASSERT_EQUAL(EXP, RES);
+    }
 }
