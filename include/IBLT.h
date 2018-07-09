91a012b80ffff5ca1ab18e262cc29fb65cea5401
admin <admin@nat-wireless-guest-reg-153-50.bu.edu>
2018-07-09 10:17:53 -0400

    initial commit

---
+++ include/IBLT.h
@@ -0,0 +1,19 @@
+//
+// Created by eliezer pearl on 7/9/18.
+//
+
+#ifndef CPISYNCLIB_IBLT_H
+#define CPISYNCLIB_IBLT_H
+
+
+class IBLT {
+    /*
+     * IBLT (Invertible Bloom Lookup Table) is a data-structure designed to add partial invertibility to the standard
+     * Bloom Filter data-structure.
+     *
+     * The IBLT was designed by Michael T. Goodrich and Michael Mitzenmacher in 
+     */
+};
+
+
+#endif //CPISYNCLIB_IBLT_H
---
+++ src/IBLT.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 7/9/18.
+//
+
+#include "IBLT.h"
